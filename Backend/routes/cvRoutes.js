a64a8b64e97974858d51c3261d7d14953b8285e3
Chayakorn <kittikoonaim@gmail.com>
2023-04-26 11:54:57 +0700

    first commit

---
+++ Backend/routes/cvRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const cvController = require("../controller/cvController");
+const router=express.Router();
+
+router.get("/auth_app", cvController.authApp);
+router.get("/access_token", cvController.accessToken);
+router.get("/get_courses",cvController.getCourses);
+router.get("/get_course_assignments",cvController.getCourseAssignments);
+router.get("/get_item_assignment",cvController.getItemAssignment);
