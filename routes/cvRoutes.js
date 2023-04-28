b0f155f48988291403b5e58c5be8e23b7f26efc9
Chayakorn <kittikoonaim@gmail.com>
2023-04-28 22:12:51 +0700

    add get info route

--- routes/cvRoutes.js
+++ routes/cvRoutes.js
@@ -5,5 +5,6 @@ const router=express.Router();
 router.get("/auth_app", cvController.authApp);
 router.get("/access_token", cvController.accessToken);
 router.get("/get_courses",cvController.getCourses);
-router.get("/get_course_assignments",cvController.getCourseAssignments);
-router.get("/get_item_assignment",cvController.getItemAssignment);
+router.get("/get_user_info",cvController.getUserInfo);
+// router.get("/get_course_assignments",cvController.getCourseAssignments);
+// router.get("/get_item_assignment",cvController.getItemAssignment);
